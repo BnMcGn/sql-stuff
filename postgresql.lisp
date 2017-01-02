a9eb696f8f874a2f44c0074a5fc1ee354eeb43e9
bnmcgn@gmail.com <bnmcgn@gmail.com>
2017-01-02 06:12:11 -0800

    Formatting changes

--- postgresql.lisp
+++ postgresql.lisp
@@ -9,68 +9,68 @@
 
 (defmethod %fulltext-where (text cols (database (eql :postgresql)))
   (let ((clauses
-	 (collecting
-	     (dolist (col (ensure-list cols))
-	       (collect 
-		   (sql-expression 
-		    :string
-		    (format nil "to_tsvector(~a) @@ to_tsquery('~a')"
-			    (col-from-attribute-obj col)
-			    (escape-sql-string text))))))))
+   (collecting
+       (dolist (col (ensure-list cols))
+         (collect
+       (sql-expression
+        :string
+        (format nil "to_tsvector(~a) @@ to_tsquery('~a')"
+          (col-from-attribute-obj col)
+          (escape-sql-string text))))))))
     (list :where
-	   (if (< 1 (length clauses)) 
-		   (apply #'sql-or clauses)
-		   (car clauses)))))
+     (if (< 1 (length clauses))
+       (apply #'sql-or clauses)
+       (car clauses)))))
 
 (defmethod %get-table-pkey (table (database (eql :postgresql)))
   (declare (ignore database))
     (with-a-database nil
       (grab-one
-       (select 
-	(sql-expression :string "pg_catalog.pg_attribute.attname") 
-	:from (list
-	       (colm 'pg_catalog 'pg_attribute)
-	       (colm 'pg_catalog 'pg_constraint))
-	:where
-	(sql-and
-	 (sql-= (colm 'attrelid) (relation-oid-sql table))
-	 (sql-= 
-	  (sql-expression :string "pg_catalog.pg_attribute.attnum")
-	  (sql-expression 
-	   :string "pg_catalog.pg_constraint.conkey[1]"))
-	 (sql-=
-	  (sql-expression :string "pg_catalog.pg_constraint.contype") "p")
-	 (sql-=
-	  (sql-expression :string "pg_catalog.pg_constraint.conrelid")
-	  (relation-oid-sql table)))))))
+       (select
+  (sql-expression :string "pg_catalog.pg_attribute.attname")
+  :from (list
+         (colm 'pg_catalog 'pg_attribute)
+         (colm 'pg_catalog 'pg_constraint))
+  :where
+  (sql-and
+   (sql-= (colm 'attrelid) (relation-oid-sql table))
+   (sql-=
+    (sql-expression :string "pg_catalog.pg_attribute.attnum")
+    (sql-expression
+     :string "pg_catalog.pg_constraint.conkey[1]"))
+   (sql-=
+    (sql-expression :string "pg_catalog.pg_constraint.contype") "p")
+   (sql-=
+    (sql-expression :string "pg_catalog.pg_constraint.conrelid")
+    (relation-oid-sql table)))))))
 
 (defmethod %insert-record ((database (eql :postgresql)) table values)
-  (trycar 
+  (trycar
    'caar
    (with-a-database ()
-     (clsql-sys:query 
+     (clsql-sys:query
       (concatenate 'string
-		   (clsql-sys::sql-output 
-		    (clsql-sys::make-sql-insert
-		     :into (sql-expression :table table)
-		     :av-pairs
-		     (mapcar (lambda (x)
-			       (list (intern (symbol-name (car x)))
-				     (if (equal (cdr x) "") nil (cdr x))))
-			     values)))
-		   (format nil 
-			   " returning ~(~a~)"
-			   (get-table-pkey table)))))))
+       (clsql-sys::sql-output
+        (clsql-sys::make-sql-insert
+         :into (sql-expression :table table)
+         :av-pairs
+         (mapcar (lambda (x)
+             (list (intern (symbol-name (car x)))
+             (if (equal (cdr x) "") nil (cdr x))))
+           values)))
+       (format nil
+         " returning ~(~a~)"
+         (get-table-pkey table)))))))
 
-(defmethod %next-val ((database (eql :postgresql)) sequence) 
-  (car (query 
-	(strcat "select " 
-		(clsql-sys::sql-output (sql-function "nextval" sequence)))
-	:flatp t)))
+(defmethod %next-val ((database (eql :postgresql)) sequence)
+  (car (query
+  (strcat "select "
+    (clsql-sys::sql-output (sql-function "nextval" sequence)))
+  :flatp t)))
 
+;;;FIXME: Whole package thing.
 
-
-(defmethod %fulltext-where 
+(defmethod %fulltext-where
     (text cols (database clsql-postgresql-socket3:postgresql-socket3-database))
   (%fulltext-where text cols :postgresql))
 
@@ -89,7 +89,9 @@
      sequence)
   (%next-val :postgresql sequence))
 
-(defmethod %fulltext-where 
+
+
+(defmethod %fulltext-where
     (text cols (database clsql-postgresql:postgresql-database))
   (%fulltext-where text cols :postgresql))
 
@@ -103,8 +105,7 @@
      table values)
   (%insert-record :postgresql table values))
 
-  (defmethod %next-val
-      ((database clsql-postgresql:postgresql-database)
-       sequence)
-    (%next-val :postgresql sequence))
-
+(defmethod %next-val
+    ((database clsql-postgresql:postgresql-database)
+     sequence)
+  (%next-val :postgresql sequence))
